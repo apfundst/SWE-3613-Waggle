f72ffe62e76a1e62bd55ebbca324850fc6a065af
firefox0102 <firefox0102@gmail.com>
2014-01-29 21:18:27 -0500

    Finalized File Upload Changes without database interaction
    File now uploads to local folder successfully, and renames itself if
    there is already a file there with the same name

--- fileupload.php
+++ fileupload.php
@@ -1,4 +1,50 @@
 <?php
+function rename_file($file_name, $counter)
+  {
+    $extension = strpos($file_name, '.');
+    if ($extension === false)
+    {
+      //if there is no extension
+      echo "No File Extension Found";
+      $new_ext = "";
+    }
+
+    $new_ext = substr($file_name, $extension);
+    //$new_file_name = $_FILES["file"]["name"] . $new_ext;
+    $path_parts = pathinfo($file_name);
+    $new_filename = $path_parts['filename'] . $counter . $new_ext;
+    echo $new_filename;
+    return $new_filename;
+  }
+
+  function upload_file_check($file_name, $counter)
+  {
+    echo "filename: " . $file_name . $counter;
+    $temp_name1 = rename_file($file_name, $counter);
+    //check if it exists
+    if (file_exists("upload/" . $temp_name1))
+    {
+      //if it does exist, counter++ and try again
+      $counter = $counter + 1;
+      echo "counter: " . $counter;
+      upload_file_check($file_name, $counter);
+    }
+    else
+    {
+      $temp_name = rename_file($file_name, $counter);
+      //file doesn't exist, successfully uploads
+      move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $temp_name);
+       echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+       //
+       //*********
+       // need to store in database
+       //
+       //*********
+       //
+    }
+    
+  }
+
 if ($_FILES["file"]["error"] > 0)
   {
   echo "Error: " . $_FILES["file"]["error"] . "<br>";
@@ -12,29 +58,11 @@ else
   echo "Type: " . $_FILES["file"]["type"] . "<br>";
   echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
   
- upload_file_check($_FILES["file"]["name"], 0);
+  upload_file_check($_FILES["file"]["name"], 0);
 
-      //header("Location: /index.php");
+  //header("Location: /index.php");
   }
 
-  function upload_file_check($file_name, $counter)
-  {
-    //check if it exists
-    echo "filename: " . $file_name . $counter;
-    if (file_exists("upload/" . ($file_name . $counter)))
-    {
-      //if it does exist, counter++ and try again
-      $counter = $counter + 1;
-      echo "counter: " . $counter;
-      upload_file_check($file_name, $counter);
-    }
-    else
-    {
-      //file doesn't exist, successfully uploads
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-       "upload/" . $_FILES["file"]["name"];
-       echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-    }
-    
-  }
+  
+  
 ?>
