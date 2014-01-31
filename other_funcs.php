a04abaf00e6a8b9e42425107afbed57cedca64ef
jcathcart1 <jcathcart1@gmail.com>
2014-01-31 00:06:58 -0500

    do get groups function

--- other_funcs.php
+++ other_funcs.php
@@ -28,43 +28,41 @@ $sql_query = "
 ";
 }
 
+
 func do_get_groups($email){
 	// gets groups from db for user
-
 
 	// get the group_name from the group table
 	// where the email matches the email in the membership table
 	// and the group_id matches the group_id in the group table
-	$sql_query = "
-		   SELECT group_id
-		   FROM membership
-		   WHERE $e
-
-
+	$sql = "
 		   SELECT group.group_name 
 		   FROM group
 		   INNER JOIN membership
 		   ON group.group_id = membership.group_id 
-		   AND $email =   
-
+		   AND $email = membership.email   
 		   ORDER BY group.group_name ASC		
-";
-
+	";
+	$result = mysql_query(sql);
+     // Get the information from the result set
+    return mysql_fetch_assoc($result);
 }
-
-
-
 
 
 func do_get_threads($group){
 // gets threads from group from db
 
-
-
-
-
-
-
+// get the thread_name from the group table
+// where the email matches the email in the membership table
+// and the group_id matches the group_id in the group table
+$sql_query = "
+		   SELECT thread.thread_name 
+		   FROM group
+		   INNER JOIN membership
+		   ON group.group_id = membership.group_id 
+		   AND $email = membership.email   
+		   ORDER BY group.group_name ASC		
+";
 
 }
 
