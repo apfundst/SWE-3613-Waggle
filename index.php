575196043a7bd6c508e2dc2cedec91a6bba0d87a
Andrew Pfundstein <drew@myskiprofile.com>
2014-01-30 21:44:30 -0500

    other things

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION["email"])) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Waggle | Student Solution</title>
 <link rel="stylesheet" type="text/css" href="css.css">
 </head>
 
