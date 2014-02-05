bb0cb1e8a83b955dc441dc81f63657e5080507b2
jcathcart1 <jcathcart1@gmail.com>
2014-02-04 20:21:13 -0500

    More admin funcs

--- admin_funcs.php
+++ admin_funcs.php
@@ -3,25 +3,54 @@ include_once('connection.php');
 
 function do_admin_get_groups(){
 	// Returns all groups
-
 	$sql = "
 		SELECT group_id, group_name, creator
 		FROM `group`
-		
-
-
 	";
+	$result = mysql_query($sql);
+	if(!$result){
+		die("Invalid query: " .mysql_error());
+	}	
+	else{
+     	// Get the information from the result set
+		$i = 0;
+     	while($row = mysql_fetch_row($result)){
+     		$data[$i] = $row;
+     		$i++; 
+     	}
+    	return $data;
+    }
+    	
+}
 
+function do_admin_remove_group(){
 
 }
 
+function do_admin_ban_user_site(){
+	// Time based ban
 
+}
 
+function do_admin_ban_group(){
+	// Time based ban
+}
 
+function do_admin_ban_user_group(){
+	// Time based ban
+}
 
+function do_admin_remove_file(){
 
+}
 
+function do_admin_remove_post(){
 
+}
+
+function do_admin_remove_thread(){
+
+}
 
 
 
