eb7771c767f4b2e931abc9700f6a67f301f0b1e0
firefox0102 <firefox0102@gmail.com>
2014-02-09 19:50:40 -0500

    Added user_remove_funcs

--- other_funcs.php
+++ other_funcs.php
@@ -2,5 +2,5 @@
 // include_once('connection.php');
 include('user_create_funcs.php');
 include('user_get_funcs.php');
-
+include('user_remove_funcs.php');
 ?>
---
+++ user_remove_funcs.php
@@ -0,0 +1,55 @@
+<?
+include_once('connection.php');
+
+//Delete Messages (only if admin or creator)
+function do_remove_message($email, $message_id, $creator)
+{
+	//function do_remove_message($email, $message_id, $creator, $admin)
+	// Deletes messages from the database
+	//if email matches the creator OR if admin
+	//if($email == $creator || $admin = true)
+	if($email == $creator)
+	{
+		$sql = "
+			DELETE FROM `messages`
+			WHERE '$creator' = creator AND'$message_id' = message_id
+		";
+		$result = mysql_query($sql);
+		if (!$result) {
+			mysql_query('ROLLBACK');
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+	}
+}
+
+//Delete Files (only if admin or creator)
+function do_remove_file($email, $file_name_path, $creator)
+{//function do_remove_file($email, $file_name_path, $creator, $admin)
+	// Deletes files from the database
+	//if email matches the creator OR if admin
+	//if($email == $creator || $admin = true)
+	if($email == $creator)
+	{
+		$sql = "
+			DELETE FROM `file`
+			WHERE '$creator' = creator AND '$file_name_path' = $file_name_path
+		";
+		$result = mysql_query($sql);
+		if (!$result) {
+			mysql_query('ROLLBACK');
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+	}
+}
+
+//Ban Method
+
+?>
