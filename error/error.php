1e555b59d40ef63ea83a49872f6fe435e10f9c09
Andrew Pfundstein <drew@myskiprofile.com>
2014-02-16 20:22:08 -0500

    error page
    Mitchell's code

---
+++ error/error.php
@@ -0,0 +1,25 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>WOAH!!</title>
+<link rel="stylesheet" type="text/css" href="LOGIN/css.css">
+</head>
+<body>
+	<div class="error-block">
+		<center>OOPS!</center>
+	</div>
+	<div class="error-here">
+		<center>Problem</center>
+	</div>
+	<div class="IMG">
+		<center>
+			<img class="img-broken"height="234px"width="272px"src="broken.png">
+		</center>
+	</div>
+	<div class="panel-click">
+		<center>
+			<a href="admin.php">Click here to return Home</a>
+		</center>
+	</div>
+</body>
+</html>
