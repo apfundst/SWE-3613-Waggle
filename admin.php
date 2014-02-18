a1abf46b9d43029f4f85081b65e7f0ff877f3365
firefox0102 <firefox0102@gmail.com>
2014-02-18 18:11:39 -0500

    Fixed row

--- admin.php
+++ admin.php
@@ -23,7 +23,7 @@ else
     }
   }
 }
-  
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -53,7 +53,6 @@ else
 
     </div>
     </div> 
-    </div>
 </div>
 
 <div class="col-lg-4">
