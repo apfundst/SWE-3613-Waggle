22423ed30ef8b84239d939df97348842934fbe7f
firefox0102 <firefox0102@gmail.com>
2014-02-18 13:29:56 -0500

    .

---
+++ ban_user.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include 'admin_funcs.php';
+if(!isset($_SESSION["email"])) {
+  header('Location: http://www.waggle.myskiprofile.com/login.php');
+  exit();
+}
+else{
+if($_POST['member_email']){
+	$return_bool = do_admin_ban_user($_POST['member_email']);
+	if($return_bool == TRUE){
+		header('Location: http://www.waggle.myskiprofile.com/admin.php');
+  exit();
+	}
+	else{
+		echo "mucho errors. Please contact the web admin!";
+	}
+}
+}
+
+
+
+?>
