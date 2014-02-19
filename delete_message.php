314031f5589499003b3eb786980eee0324e6bbd1
Andrew Pfundstein <drew@myskiprofile.com>
2014-02-18 23:29:39 -0500

    delete file

--- delete_message.php
+++ delete_message.php
@@ -1,22 +1,25 @@
 <?php
 session_start();
-include 'admin_funcs.php';
+include 'other_funcs.php';
+ob_start();
 if(!isset($_SESSION["email"])) {
   header('Location: http://www.waggle.myskiprofile.com/login.php');
   exit();
 }
 else{
-if($_POST['message_id']){
-	$return_bool = do_remove_message($_POST['message_id']);
-	if($return_bool == TRUE){
-		header('Location: http://www.waggle.myskiprofile.com/thread.php');
-  exit();
+	if($_POST['message_id']){
+		$return_bool = do_remove_message($_POST['message_id']);
+		if($return_bool == TRUE){
+			header('Location: http://www.waggle.myskiprofile.com/thread.php');
+	  		exit();
+	  		ob_flush();
+		}
+		else{
+			header('Location: http://www.waggle.myskiprofile.com/error/error.php?err=Message%20could%20be%20deleted');
+	  		exit();
+	  		ob_flush();
+		}
 	}
-	else{
-		header('Location: http://www.waggle.myskiprofile.com/error/error.php?err=Message%20could%20be%20deleted');
-  exit();
-	}
-}
 }
 
 ?>
